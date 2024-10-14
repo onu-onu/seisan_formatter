9230c36a106515d146174402a5c5c92c340eb3d5
tomoya-onuki <tomoya.onuki28@gmail.com>
2024-10-14 23:41:32 +0900

    Lineに貼り付けた時に崩れないように修正

--- src/main.ts
+++ src/main.ts
@@ -23,17 +23,24 @@ function formating() {
         console.log(`${label} ${value}`);
         outdiv.innerHTML += `${label} ${value}<br />`;
     });
-
+    
+    let outputStr0 = 'と→み<br />';
+    let outputStr1 = 'み→と<br />';
     lines.slice(6, lines.length - 1).forEach(line => {
         let tokens = line.split('\t');
         let use = formatString(tokens[0], '　');
         let date = tokens[1].slice(5);
         let price = tokens[2];
         let who = tokens[3];
-        let outputStr = `${use}　${date}　${who}　${price}<br />`;
-        console.log(outputStr);
-        outdiv.innerHTML += outputStr;
+        if (who == 'と→み') {
+            outputStr0 += `${use}　${date}　${price}<br />`;
+        } else if (who == 'み→と') {
+            outputStr1 += `${use}　${date}　${price}<br />`;
+        }
     });
+    outdiv.innerHTML += outputStr0;
+    outdiv.innerHTML += '<br>';
+    outdiv.innerHTML += outputStr1;
 }
 
 function formatString(input: string, fullWidthSpace: string): string {
